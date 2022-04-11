3f48f6d21d60d4ea1026839c97dbae159638bdd1
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-11 14:20:55 -0500

    feat: Implemented getting Sources and Headlines from NewsAPI. Also added providers and Material Modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {NewsApiService} from "./core/services/news-api.service";
+import {LogoApiService} from "./core/services/logo-api.service";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
@@ -13,9 +22,16 @@ import { HttpClientModule } from "@angular/common/http";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [NewsApiService, LogoApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/services/news-api.service.ts
+++ src/app/core/services/news-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
+  apiKey = 'fecf4feeffa64e4da682e7d268612ce5';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  initArticles() {
+    return this.getArticlesBySourceId('bbc-news');
+  }
+
+  getSources() {
+    return this.http.get(`https://newsapi.org/v2/top-headlines/sources?apiKey=${this.apiKey}`);
+  }
+
+  getArticlesBySourceId(sourceId: string) {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`);
+  }
+
+
+
+
 }
